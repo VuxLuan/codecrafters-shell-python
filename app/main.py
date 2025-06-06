7ce2510c70dfc97a87833188c9df39ccb096177a
Luan <vuxluan@gmail.com>
2025-06-06 13:56:44 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    
+    builtins: list[str] = ["echo", "exit", "type"]
 
     # Wait for user input
     while(True):
@@ -14,6 +14,11 @@ def main():
             break
         elif parts[0] == "echo":
             print(" ".join(parts[1:]))
+        elif parts[0] == "type":
+            if parts[1] in builtins:
+                print(f"{parts[1]} is a shell builtin")
+            else:
+                print(f"{parts[1]}: not found")
         else:
             print(f"{command}: command not found")
         
