0fe41df1601a2e82bbc117dcaf280634deff58db
Luan <vuxluan@gmail.com>
2025-06-06 08:50:17 +0700

    exit

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     while(True):
         sys.stdout.write("$ ")
         command = input()
-        if command == "0":
+        if command == "exit 0":
             break
         print(f"{command}: command not found")
         
